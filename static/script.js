e9c503990eb85f185c39dafc0509e9ee2db318e4
basantetlhive <basantkumar.etlhive@gmail.com>
2024-09-15 19:59:14 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
 
+function startRecognition() {
+    fetch('/process', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('resultText').innerText = "Recognized Text: " + data.text;
+        document.getElementById('resultClassification').innerText = "Classification: " + data.classification;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('resultText').innerText = "Error occurred!";
+    });
+}
